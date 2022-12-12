cbd776ee37cc83bd9e9ce956c81622b4bf628329
masikdev <masikroosevelt@gmail.com>
2022-12-12 19:32:45 +0700

    mengubah spesifikasi

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="spesifikasi.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">Spesifikasi</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="spesifikasi.php">Data Spesifikasi</a></li>
+                            <li><a class="dropdown-item" href="spesifikasi.php">Data Spesifikasi Masik</a></li>
                             <li>
                                 <hr class="dropdown-divider" />
                             </li>
